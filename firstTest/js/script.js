b25f2813c343f2b043a83e06248cfba887b5e4a2
Victor <mr.dyrban@gmail.com>
2021-06-15 20:13:51 +0300

    added task for lesson12

--- firstTest/js/script.js
+++ firstTest/js/script.js
@@ -1,7 +1,17 @@
 'use strict'
-const obj = {
-    name: 'jhon',
-    age: 25,
-    isMarr: false
+let numberOfFilms = prompt("сколько фильмов вы смотрели?",10);
+
+let obj = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
 };
+let a = prompt("какой последний фильм вы смотрели");
+let b = prompt("на сколько оцените его?");
+let c = prompt("какой последний фильм вы смотрели");
+let d = prompt("на сколько оцените его?");
+obj.movies[a] = b;
+obj.movies[c] = d;
 console.log(obj);
