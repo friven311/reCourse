3376f0837af7d00e3994feee32ab736c2b241ce0
Victor <mr.dyrban@gmail.com>
2021-06-15 21:04:20 +0300

    added lesson 15 task

--- firstTest/js/script.js
+++ firstTest/js/script.js
@@ -1,6 +1,12 @@
-'use strict'
+'use strict';
 let numberOfFilms = prompt("сколько фильмов вы смотрели?",10);
-
+if(numberOfFilms < 10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if(numberOfFilms > 10 && numberOfFilms < 30){
+    console.log("Вы классический зритель");
+} else if(numberOfFilms > 30){
+    console.log("Вы киноман");
+}
 let obj = {
     count:numberOfFilms,
     movies:{},
@@ -8,10 +14,17 @@ let obj = {
     genres:[],
     privat:false
 };
-let a = prompt("какой последний фильм вы смотрели");
-let b = prompt("на сколько оцените его?");
-let c = prompt("какой последний фильм вы смотрели");
-let d = prompt("на сколько оцените его?");
-obj.movies[a] = b;
-obj.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("какой последний фильм вы смотрели?");
+    let b = prompt("на сколько оцените его?");
+    if (a != null && a !== "" && a.length < 50 && b != null && b !== ""){
+        
+        obj.movies[a] = b;
+    }else{
+        i--;
+    }
+    
+    
+}
 console.log(obj);
